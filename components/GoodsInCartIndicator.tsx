06c899fc0272c83acabb64d218edf9f3e6cf079f
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-11-21 22:07:12 +0200

    Update GoodsInCartIndicator.tsx

--- components/GoodsInCartIndicator.tsx
+++ components/GoodsInCartIndicator.tsx
@@ -4,9 +4,11 @@ const GoodsInCartIndicator = () => {
   const { cart } = useGlobalContext();
 
   return (
-    <div className="absolute top-[-10px] right-[-10px] w-[25px] h-[25px] rounded-full bg-red-600 flex justify-center items-center">
-      <span className="text-white font-bold">{cart?.length}</span>
-    </div>
+    cart.length > 0 && (
+      <div className="absolute top-[-10px] right-[-10px] w-[25px] h-[25px] rounded-full bg-red-600 flex justify-center items-center">
+        <span className="text-white font-bold">{cart?.length}</span>
+      </div>
+    )
   );
 };
 
