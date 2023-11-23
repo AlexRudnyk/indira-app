2c304b85580950af2e41a6cfa00763df37aa7ebd
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-11-23 18:17:05 +0200

    adds counter to cart item

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -5,19 +5,30 @@ import { GoodProps } from "@/types";
 import Image from "next/image";
 
 const CartItem = ({ good }: { good: GoodProps }) => {
+  const [count, setCount] = useState<number>(1);
+
   return (
     <li className="mb-8">
-      <div className="flex rounded-lg shadow-[7px_15px_20px_0px_rgba(0,0,0,0.6)] p-2">
+      <div className="flex rounded-lg shadow-[7px_15px_20px_0px_rgba(0,0,0,0.6)] p-1">
         <Image
           src={good.photoURL}
           alt="good in cart"
-          width={100}
-          height={100}
+          width={120}
+          height={120}
           className="mr-6 rounded-lg"
         />
-        <div>
+        <div className="w-[600px]">
           <h1 className="text-xl font-bold mb-3">{good.title}</h1>
           <p>{good.text}</p>
+        </div>
+        <div className="flex items-center w-[200px] ml-auto">
+          <button className="bg-gray-300 w-[40px] h-[40px]  rounded-md flex justify-center items-center">
+            <span className="text-2xl">-</span>
+          </button>
+          <p className="mx-4 text-2xl">{count}</p>
+          <button className="bg-gray-300 w-[40px] h-[40px]  rounded-md flex justify-center items-center">
+            <span className="text-2xl">+</span>
+          </button>
         </div>
       </div>
     </li>
